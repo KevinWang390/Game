15b2ea632544c94cda6678de280100135ce250c5
kevis <kw6ds@virginia.edu>
2023-05-26 12:08:31 -0400

    work on menu

--- Game 2/Menu.h
+++ Game 2/Menu.h
@@ -3,7 +3,6 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <fstream>
 
 #include "Graphics.h"
 #include "Inputable.h"
@@ -12,6 +11,7 @@
 
 class Page;
 class Overlay;
+class Menu;
 
 class PageElement {
 	bool selected;
@@ -23,7 +23,7 @@ public:
 	PageElement() {
 		selected = false;
 		x = y = 0.0f;
-		name = "none";
+		name = "";
 	};
 	PageElement(float xpos, float ypos, std::string n) {
 		selected = false;
@@ -38,15 +38,27 @@ public:
 
 class Link : public PageElement {
 	std::string target;
-	bool selected;
 public:
-	int what_type() { return LINK; }
-	void move() {} // TODO:
-	void* click(WPARAM wParam) {
-		if (wParam == VK_RETURN) return (void*)&target;
+	Link() {
+		target = "";
 	}
-	void draw() override {} // TODO: draw code goes here
+	Link(std::string t, float xpos, float ypos, std::string n) : PageElement(xpos, ypos, n) {
+		target = t;
+	}
+	int what_type() override { return LINK; }
+	void move() override {} // TODO:
+	void* click(WPARAM wParam) override {
+		if (wParam == VK_RETURN) return (void*)&target;
+		return nullptr;
+	}
+	void draw() override {
+		Graphics::rtarget->FillRoundedRectangle(D2D1::RoundedRect(
+			D2D1::RectF(x, y, x + 100.0f, y + 50.0f), 10.0f, 10.0f),
+			Graphics::getSolidColorBrush("yellow"));
+	} // TODO: draw code goes here
 };
+
+class Button : public PageElement {}; // will likely need to invoke some other file for game-affecting functions
 
 class Tab : public PageElement {}; // TODO: decide if I need this at all
 
@@ -83,15 +95,29 @@ class Overlay : public PageElement {}; // TODO: know how to do this
 class Page {
 	// parent class
 	std::string name;
-	int ID;
-	PageElement* entryElement; // initial selection upon entry (not meant to be changed on runtime)
+	int ID; // dunno what i'm gonna use this for
 	PageElement* currentElement; // current page element selected
-	std::vector<PageElement*> elements; // draw the page elements
+	std::vector<PageElement*> elements; // draw the page elements, element 0 will be the entry element
 	Overlay* overlay; // the current overlay (will be nullptr if no overlay is active)
 
 	friend class PageElement;
+	friend class Menu;
 public:
-	Page() {}
+	Page() {
+		name = "";
+		ID = -1;
+		currentElement = nullptr;
+		elements = {};
+		overlay = nullptr;
+	}
+	Page(std::string n, int id, std::vector<PageElement*>& e, Overlay* o) {
+		name = n;
+		ID = id;
+		elements = e;
+		overlay = o;
+		if (elements.size()) currentElement = elements[0];
+		else currentElement = nullptr;
+	}
 	void enterPage() {} // enters page, reinitializes currentElement
 	void drawPage() {
 		Graphics::rtarget->FillRoundedRectangle(
@@ -116,15 +142,26 @@ public:
 		entryPage = currentPage = nullptr;
 		backtrace = {};
 	}
+	void add_page(Page* p, bool entry) {
+		if (entry) entryPage = p;
+		page_table[p->name] = p;
+	}
 	void draw() {
 		if (InputController::get_mode() != VK_TAB) return;
 		Graphics::rtarget->GetTransform(&transform);
 		Graphics::rtarget->SetTransform(D2D1::IdentityMatrix());
-		// actual drawing goes here
+		if (currentPage) currentPage->drawPage();
 		Graphics::rtarget->SetTransform(transform);
 	};
 	void input_start(WPARAM wParam) {
 		if (InputController::get_mode() != VK_TAB) return;
+		switch (wParam) {
+		case VK_TAB:
+			currentPage = entryPage; // TODO: sort out the page's entry element
+			break;
+		default:
+			break;
+		}
 	}
 	void input_end(WPARAM wParam) { return; }
 };
@@ -133,6 +170,10 @@ public:
 
 Menu* get_field_menu() { // TODO: hardcode the menu creator
 	Menu* m = new Menu();
+	std::vector<PageElement*> p_elist;
+	p_elist.push_back(new Link("none", 30.0f, 30.0f, "dummy"));
+	Page* p = new Page("entry", 0, p_elist, nullptr);
+	m->add_page(p, true);
 	return m;
 }
 
